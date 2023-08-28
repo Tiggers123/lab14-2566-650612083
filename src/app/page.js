345bd7584f2d1aa7e33f67ddd6d7c1f03dd74d41
tigerlab <tiggers12@hotmail.com>
2023-08-28 20:11:32 +0700

    นาาา

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,53 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import {
+  Container,
+  Rating,
+  Text,
+  Title,
+  Textarea,
+  Button,
+  Divider,
+  Group,
+  Pagination,
+} from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
-
+      <Title order={4} mt={"sm"}>
+        Your rating
+      </Title>
+      <Rating size="lg" />
+      <Textarea
+        size="sm"
+        label="Your review"
+        placeholder="Do you enjoy eating?"
+        mt={"xs"}
+        minRows={"3"}
+      />
+      <Button color="orange" mt={"xs"} size="sm">
+        Submit Review
+      </Button>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title order={4}>Elon Musk</Title>
+        <Rating size="sm" defaultValue={5} />
+      </Group>
+      <Text align="center" color="dimmed">
+        Best pizza in this world. I give you X score.
+      </Text>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title order={4}>Mark Zuck</Title>
+        <Rating size="sm" defaultValue={4} />
+      </Group>
+      <Text align="center" color="dimmed">
+        My favourite part is pepperoni.
+      </Text>
+      <Pagination total={20} color="orange" position="center" mt={"md"} />
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Tiger Tanner 650612083
       </Text>
     </Container>
   );
